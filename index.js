c45f2787f15ede77d2a4be3a9ef587f0be7464f4
David Clark <dlc.clark@gmail.com>
2018-01-09 03:06:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,25 +2,21 @@ function shout(string){
   return string.toUpperCase()
 }
 
-'Hello!'.toUpperCase()
 
 function whisper(string){
   return string.toLowerCase()
 }
 
-'Hello'.toLowerCase()
 
 function logShout(string){
   console.log('Hello'.toUpperCase())
 }
 
-'Hello'.toUpperCase()
 
 function logWhisper(string){
   console.log('Hello'.toLowerCase())
 }
 
-'Hello'.toLowerCase()
 
 function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
