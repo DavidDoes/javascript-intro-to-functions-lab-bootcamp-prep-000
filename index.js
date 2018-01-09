30f649a79ec5fbef0dd474ac38e35a392f27c34e
David Clark <dlc.clark@gmail.com>
2018-01-09 02:32:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string.toLowerCase() === lowercase){
     return "I can't hear you!"
-  } else if (string.toUpperCase()) {
+  } else if (string.toUpperCase() === uppercase) {
     return "YES INDEED!"
   } else {
     return "I love you, too."
